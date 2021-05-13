22ffa87a6bf238461503b06c19f4c54bcc178dcd
Vullbyte <oto10tis@outlook.jp>
2021-05-13 13:48:07 +0000

    [Add]検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,18 @@ class User < ApplicationRecord
   def unfollow(relathinoship_id)
     relationships.find(relathinoship_id).destroy!
   end
+  
+  def self.search(search,word)
+    if search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      @user = User.where("#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
