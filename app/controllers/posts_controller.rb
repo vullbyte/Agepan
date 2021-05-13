84753756b35506a140ef9b677ce8e95ecc7ee01f
Vullbyte <oto10tis@outlook.jp>
2021-05-13 13:05:09 +0000

    コメント機能にもAjax追加、しかし最後が怪しい

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @post_comment = PostComment.new
+    @post_comments = @post.post_comments.all
   end
 
   def destroy
