9cee1abc1af78bac36a0efff346bd76dbce7f5d3
Vullbyte <oto10tis@outlook.jp>
2021-05-16 07:10:17 +0000

    [Add]ユーザページへのお気に入り一覧機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts
     
-    favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
-    @favorite_list = Post.find(favorites)     # postsテーブルから、お気に入り登録済みのレコードを取得
+    # favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
+    # @favorite_list = Post.find(favorites)     # postsテーブルから、お気に入り登録済みのレコードを取得
+    @favorites = Favorite.where(user_id: @user)
+  
   end
   
   def edit
